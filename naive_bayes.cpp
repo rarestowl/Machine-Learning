7d773330ee6202573cf963f9a7181a605745b3db
Bubo Howler <49031316+rarestowl@users.noreply.github.com>
2019-08-08 19:04:16 +0530

    Create naive_bayes.cpp

---
+++ naive_bayes.cpp
@@ -0,0 +1,25 @@
+/*
+document  class   words                                               
+d1         C      chinese
+d2         C      chinese
+d3         C      chinese
+d4         J      chinese
+d5         ?      chinese chinese chinese
+*/
+#include <bits/stdc++.h>
+using namespace std;
+struct Docs{
+  int class_type;
+  int total_words;
+  int unique_words;
+  map<string, int> freq;
+};
+Docs document[5];
+void enter_data(){
+  
+}
+int main(){
+  document[0].total_words = 
+  
+  return 0;
+}
